511c344fd87d637b09ec0cac0667487d91fa7ac3
mark miiro <markmiiro77@gmail.com>
2025-09-14 16:13:17 +0300

    Complete basic arithmetic exercise

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,13 @@
+# future_age_calculator.py
+
+# Prompt the user for their current age
+current_age = int(input("How old are you? "))
+
+# Years difference between 2050 and 2023
+years_to_add = 27
+
+# Calculate future age
+future_age = current_age + years_to_add
+
+# Print result
+print("In 2050, you will be", future_age, "years old.")
